e81253642b1761f2a80bdfe1395ee081b95e0635
tvh68 <tvh68job@gmail.com>
2021-05-12 10:47:27 +0200

    page mentions légales

--- resources/views/site/pages/mentionslegales.blade.php
+++ resources/views/site/pages/mentionslegales.blade.php
@@ -4,5 +4,47 @@
 @endphp
 
 @section('content')
-contenu de ma page mentions légales
+<main class="container">
+    <div class="bg-light p-5">
+      <h1 class="text-center text-cannelle fw-bold bg-pale p-3 mb-5">Mentions légales</h1>
+      <h2 class="text-cannelle fw-bold">Coordonnées</h2>
+      <ul class="icon-list p-0">
+        <li><p><i class="fas fa-user text-warning"></i> Patrick Nicaise</p></li>
+        <li><p><i class="fas fa-map-marker-alt text-warning"></i> Rue Jules Hans 79, 1420 Braine-l'Alleud</p></li>
+        <li><p><i class="fas fa-phone-alt text-warning"></i> 02 385 23 70</p></li>
+        <li><p class="d-flex"><i class="fas fa-envelope text-warning"></i><a href="mailto:nicaisepatrick63@gmail.com" class="text-cannelle text-decoration-none"> nicaisepatrick63@gmail.com</a></p></li>
+      </ul>
+      <h2 class="text-cannelle fw-bold">Droit d'auteur</h2>
+      <p class="lead">Le site de « Pomme Cannelle « constitue une création protégée par le droit d’auteur. Les textes, photos et autres éléments du site sont protégés par le droit d’auteur. 
+          Toute copie, adaptation, traduction, arrangement, communication au public, location et autre exploitation, modification de tout ou partie de ce site sous quelle que forme et par quel que moyen que ce soit, 
+          électronique, mécanique ou autre, réalisée dans un but lucratif ou dans un cadre privé, est strictement interdit sans mon autorisation préalable. 
+          Toute infraction à ces droits entrainera des poursuites civiles ou pénales.</p>
+      <h2 class="text-cannelle fw-bold">Marques et noms commerciaux</h2>
+      <p class="lead">Les dénominations, logos et autres signes utilisés sur ce site sont des marques et/ou noms commerciaux légalement protégés. 
+          Tout usage de ceux-ci ou de signes ressemblants est strictement interdit sans un accord préalable et écrit.</p>
+      <h2 class="text-cannelle fw-bold">Responsabilité quant au contenu</h2>
+      <p class="lead">J’apporte le plus grand soin à la création et à la mise à jour de ce site mais je ne peux toutefois pas garantir l’exactitude de l’information qui s’y trouve. 
+          Les informations contenues dans ce site pourront faire l’objet de modifications sans préavis. Les informations données sur ce site ne sauraient engager ma responsabilité, 
+          je ne pourrais être tenu pour responsable de toute omission, erreur ou lacune qui aurait pu se glisser dans ses pages ainsi que des conséquences, quelles qu’elles soient, 
+          pouvant résulter de l’utilisation des informations et indications fournies.</p>
+      <h2 class="text-cannelle fw-bold">Données personnelles traitées</h2>
+      <p class="lead">Vos données sont protégées via le règlement européen 2016/679 du Parlement européen et du Conseil du 27 avril 2016 (entré en application le 25 mai 2018) relatif à la protection des personnes physiques à l’égard du traitement des données à caractère personnel et à la libre circulation de ces données (RGPD).<br>
+        Les données personnelles désignent les données qui vous identifient en tant que personne physique. Je peux être amené à traiter les types de données personnelles suivants vous concernant : nom, prénom, adresse mail.
+        J’utilise les informations que vous me transmettez pour répondre à vos questions, traiter vos commandes, vous transmettre des informations et ne sont en aucun cas transmises à des tiers.
+        « ParadisFleuri » s’engage à protéger vos données personnelles. Nous ne procédons pas à la vente, location ou encore à des transferts de base de données à des entreprises tierces.</p>
+      <h2 class="text-cannelle fw-bold">Google Analytics</h2>
+      <p class="lead">Ce site utilise Google Analytics, un service d’analyse de site internet fourni par Google Inc. ( Google ). Google Analytics utilise des cookies , 
+          pour aider le site internet à analyser l’utilisation du site par ses utilisateurs. Les données générées par les cookies concernant votre utilisation du site (y compris votre adresse IP) 
+          seront transmises et stockées par Google sur des serveurs situés aux Etats-Unis. Google utilisera cette information dans le but d’évaluer votre utilisation du site, 
+          de compiler des rapports sur l’activité du site à destination de son éditeur et de fournir d’autres services relatifs à l’activité du site et à l’utilisation d’Internet. 
+          Google est susceptible de communiquer ces données à des tiers en cas d’obligation légale ou lorsque ces tiers traitent ces données pour le compte de Google, y compris notamment l’éditeur de ce site. 
+          Google ne recoupera pas votre adresse IP avec toute autre donnée détenue par Google. En utilisant ce site internet, vous consentez expressément au traitement de vos données nominatives par Google 
+          dans les conditions et pour les finalités décrites ci-dessus. (Consultez les règles de confidentialité ou la politique de protection de la vie privée de <a href="http://www.google.be/intl/fr/policies/" target="_blank" class="text-cannelle text-decoration-none">Google</a>).</p>
+      <h2 class="text-cannelle fw-bold">Hébergeur</h2>
+      <p class="lead">Le site est hébergé par <a href="https://www.ovh.com/be/" target="_blank" class="text-cannelle text-decoration-none">OVH</a> 2 rue Kellermann 59100 Roubaix – France.</p>
+      <h2 class="text-cannelle fw-bold">Webmaster</h2>
+      <p class="lead">Thierry Van Hiel<br />
+          https://thierryvanhiel.be/</p>
+    </div>
+  </main>
 @stop
