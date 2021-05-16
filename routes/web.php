b93c7c48a24bfaea434cbec477f4b7f9543c10f4
tvh68 <tvh68job@gmail.com>
2021-05-16 11:08:10 +0200

    ré-adaptation des routes et script js pour la map de la page contact

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,13 +13,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+// Routes des pages du site
+Route::get('/', [MainController::class, 'accueil'])->name('accueil');
+
+Route::get('/accueil', [MainController::class, 'accueil'])->name('accueil');
+
+Route::get('/restaurant', [MainController::class, 'restaurant'])->name('restaurant');
+
+Route::get('/patisserie', [MainController::class, 'patisserie'])->name('patisserie');
+
+Route::get('/contact', [MainController::class, 'contact'])->name('contact');
+
+Route::get('/mentionslegales', [MainController::class, 'mentionslegales'])->name('mentionslegales');
+
+
+//-------------------------------------------------
+//Mes tests
+Route::get('test', function(){
+    return dd(env('DB_DATABASE'));
+});
